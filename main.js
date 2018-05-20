a42582d26479aea276ac4e44bab23bab3d6fc76f
Kyle Rollins <kyleblankrollins@gmail.com>
2018-05-20 14:17:22 -0600

    05.20 Initial Vue.js layout
    Re-built the page's layout. Using Vue.js for unit selection and the unit compare section.
    
    The biggest thing left is building the unit compare Vue.js app.

---
+++ main.js
@@ -0,0 +1,35 @@
+new Vue({
+  el: '#gaalsienSelect',
+  data: {
+    items: [
+      'gaalsien #1',
+      'gaalsien #2',
+      'gaalsien #3',
+      'gaalsien #4'
+    ],
+    show: false,
+  },
+});
+
+new Vue({
+  el: '#kushanSelect',
+  data: {
+    images: [
+      { url: 'images/kushanUnits/AAV_kushan.jpg', alt: 'Kushan AAV' },
+      { url: 'images/kushanUnits/artilleryCruiser_kushan.jpg', alt: 'Kushan artillery cruiser' },
+      { url: 'images/kushanUnits/assaultCruiser_kushan.jpg', alt: 'Kushan assault cruiser' },
+      { url: 'images/kushanUnits/baserunner_kushan.jpg', alt: 'Kushan baserunner' },
+      { url: 'images/kushanUnits/battlecruiser_kushan.jpg', alt: 'Kushan battlecruiser' },
+      { url: 'images/kushanUnits/bomber_kushan.jpg', alt: 'Kushan tactical bomber'  },
+      { url: 'images/kushanUnits/carrier_kushan.jpg', alt: 'Kushan carrier'  },
+      { url: 'images/kushanUnits/gunship_kushan.jpg', alt: 'Kushan gunship'  },
+      { url: 'images/kushanUnits/interceptor_kushan.jpg', alt: 'Kushan interceptor'  },
+      { url: 'images/kushanUnits/LAV_kushan.jpg', alt: 'Kushan LAV'  },
+      { url: 'images/kushanUnits/missileBattery_kushan.jpg', alt: 'Kushan missile battery'  },
+      { url: 'images/kushanUnits/railgun_kushan.jpg', alt: 'Kushan railgun'  },
+      { url: 'images/kushanUnits/salvager_kushan.jpg', alt: 'Kushan salvager'  },
+      { url: 'images/kushanUnits/supportCruiser_kushan.jpg', alt: 'Kushan support cruiser'  }
+    ],
+    show: false,
+  },
+});
